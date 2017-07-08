312303790775d98639258b1f737a8cc6306198d0
sceint <arpanladani@gmail.com>
2017-07-08 11:08:16 +0530

    Corrected ParkingLot.sol FIle

--- contracts/ParkingLot.sol
+++ contracts/ParkingLot.sol
@@ -5,7 +5,7 @@ contract ParkingLot {
     mapping(uint => uint) public checkInTime;
     mapping(uint => uint) public checkOutTime;
 
-    //payment variables??
+    //payment variables
 
     uint public price;
     address owner;
@@ -16,39 +16,38 @@ contract ParkingLot {
     }
 
     function isRegistered(uint vNo) public returns(bool status) {
-        if(registeredVehicles[vNo] == address(0))
+        if (registeredVehicles[vNo] == address(0))
             return false;
         return true;
     }
 
     function getRegistered(uint vNo) public {
-        if(isRegistered(vNo))
+        if (isRegistered(vNo))
             return;
         registeredVehicles[vNo] = msg.sender;
     }
 
     function checkIn(uint vNo, uint inTime) public {
-        if(checkInTime[vNo] == uint(0x0) || !isRegistered(vNo))
+        if (checkInTime[vNo] == uint(0x0) || !isRegistered(vNo))
             return;
         checkInTime[vNo] = inTime;
     }
 
     function checkOut(uint vNo, uint discountPrice, uint outTime) public {
-        if(checkInTime[vNo] == uint(0x0) || checkInTime[vNo] < outTime)
+        if (checkInTime[vNo] == uint(0x0) || checkInTime[vNo] < outTime)
             return;
         checkOutTime[vNo] = outTime;
-        //payment logic??
+        //payment logic
     }
 
     function getTimeDifference(uint vNo) public returns(uint diff) {
-        if(checkOutTime[vNo] - checkInTime[vNo] <= uint(0x0))
+        if (checkOutTime[vNo] - checkInTime[vNo] <= uint(0x0))
             return 0;
         return checkOutTime[vNo] - checkInTime[vNo];
     }
-}
+
     function destroy() public {
         if (msg.sender == owner)
             suicide(owner);
     }
-}
 }
