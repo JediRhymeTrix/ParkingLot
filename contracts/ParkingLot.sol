c53d803878ec0dca981017c4b64e6ab37fff498d
sceint <arpanladani@gmail.com>
2017-07-08 14:55:10 +0530

    Resolved multiple insert issue

--- contracts/ParkingLot.sol
+++ contracts/ParkingLot.sol
@@ -3,6 +3,7 @@ pragma solidity ^ 0.4 .4;
 contract ParkingLot {
     mapping(uint => address) public registeredVehicles;
     mapping(uint => uint) public checkInTime;
+    mapping(uint => bool) public isParked;
     mapping(uint => uint) public checkOutTime;
     mapping(address => uint) public payments;
 
@@ -32,12 +33,14 @@ contract ParkingLot {
         if (checkInTime[vNo] != uint(0x0) || !isRegistered(vNo))
             return;
         checkInTime[vNo] = inTime;
+        isParked[vNo] = true;
     }
 
     function checkOut(uint vNo, uint outTime) public payable {
         if (checkInTime[vNo] == uint(0x0) || checkInTime[vNo] < outTime)
             return;
         checkOutTime[vNo] = outTime;
+        isParked[vNo] = false;
         //payment logic
         payments[msg.sender] = msg.value;
     }
