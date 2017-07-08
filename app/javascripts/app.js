1dc8013f7b47d9f6f60640c58a0b32864a39a90d
sceint <arpanladani@gmail.com>
2017-07-08 12:13:14 +0530

    contract connected to js

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -10,23 +10,24 @@ import {
 } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import conference_artifacts from '../../build/contracts/ParkingLot.json'
+import parkingLot_artifacts from '../../build/contracts/ParkingLot.json'
 
 // Conference is our usable abstraction, which we'll use through the code below.
-var Conference = contract(conference_artifacts)
 
-var accounts, account, speaker
-var conference
+// var Conference = contract(parkingLot_artifacts)
+var ParkingLot = contract(parkingLot_artifacts)
+var accounts, account
+var parkingLot
 
 function getBalance(address) {
     return web3.fromWei(web3.eth.getBalance(address).toNumber(), 'ether')
 }
 
 window.App = {
-    start: function() {
+    start: function () {
         var self = this
 
-        web3.eth.getAccounts(function(err, accs) {
+        web3.eth.getAccounts(function (err, accs) {
             if (err != null) {
                 alert('There was an error fetching your accounts.')
                 return
@@ -38,70 +39,61 @@ window.App = {
             }
 
             accounts = accs
-                // console.log(accounts);
+            // console.log(accounts);
             account = accounts[0]
-            speaker = accounts[9]
-            $('#orgBalance').html(getBalance(account))
-            $('#speakerBalance').html(getBalance(speaker))
-            self.initializeConference()
+            self.initializeParkingLot()
         })
     },
 
-    initializeConference: function() {
+    initializeParkingLot: function () {
         var self = this
-        Conference.deployed().then(function(instance) {
-            conference = instance
-            $('#confAddress').html(conference.address)
-
-            conference.location.call().then(
-                function(location) {
-                    $('#location').val(location)
-                })
-            self.checkValues()
-        }).catch(function(e) {
-            console.log(e)
+        ParkingLot.deployed().then(function (instance) {
+            parkingLot = instance
+            $('#contAddress').html(parkingLot.address)
+            $('#contBalance').html(getBalance(parkingLot.address))
+            // self.checkValues()
         })
     },
 
-    checkValues: function() {
-        Conference.deployed().then(function(instance) {
+    checkValues: function () {
+        Conference.deployed().then(function (instance) {
             conference = instance
             conference.quota.call().then(
-                function(quota) {
+                function (quota) {
                     $('input#confQuota').val(quota)
                     return conference.organizer.call()
                 }).then(
-                function(organizer) {
+                function (organizer) {
                     // console.log("organizer " + organizer);
                     $('input#confOrganizer').val(organizer)
                     return conference.numRegistrants.call()
                 }).then(
-                function(num) {
+                function (num) {
                     $('#numRegistrants').html(num.toNumber())
                     return getBalance(conference.address)
                 }).then(
-                function(balance) {
+                function (balance) {
                     $('#confBalance').html(balance)
                     return conference.speaker.call()
                 }).then(
-                function(speaker) {
+                function (speaker) {
                     $('input#speakerAddress').val(speaker)
                 })
-        }).catch(function(e) {
+        }).catch(function (e) {
             console.log(e)
         })
     },
-    changeQuota: function(val) {
+    changeQuota: function (val) {
         var conference
-        Conference.deployed().then(function(instance) {
+        Conference.deployed().then(function (instance) {
             conference = instance
             conference.changeQuota(val, {
                 from: accounts[0]
             }).then(
-                function() {
+                function () {
                     return conference.quota.call()
                 }).then(
-                function(quota) {
+                function (quota) {
                     var msgResult
                     if (quota == val) {
                         msgResult = 'Change successful'
@@ -110,22 +102,22 @@ window.App = {
                     }
                     $('#changeQuotaResult').html(msgResult)
                 })
-        }).catch(function(e) {
+        }).catch(function (e) {
             console.log(e)
         })
     },
 
-    changeLocation: function(val) {
+    changeLocation: function (val) {
         var conference
-        Conference.deployed().then(function(instance) {
+        Conference.deployed().then(function (instance) {
             conference = instance
             conference.changeLocation(val, {
                 from: accounts[0]
             }).then(
-                function() {
+                function () {
                     return conference.location.call();
                 }).then(
-                function(location) {
+                function (location) {
                     var msgResult
                     if (location == val) {
                         msgResult = 'Change successful'
@@ -135,27 +127,26 @@ window.App = {
                     }
                     $('#changeLocationResult').html(msgResult)
                 })
-        }).catch(function(e) {
+        }).catch(function (e) {
             console.log(e)
         })
     },
 
-    buyTicket: function(buyerAddress, ticketPrice) {
+    addVehicle: function (owner, vNum) {
         var self = this
-        Conference.deployed().then(function(instance) {
-            conference = instance
-            conference.buyTicket({
-                from: buyerAddress,
-                value: ticketPrice
+        ParkingLot.deployed().then(function (instance) {
+            parkingLot = instance
+            parkingLot.getRegistered(vNum, {
+                from: owner
             }).then(
-                function() {
+                function () {
                     return conference.numRegistrants.call()
                 }).then(
-                function(num) {
+                function (num) {
                     $('#numRegistrants').html(num.toNumber())
                     return conference.registrantsPaid.call(buyerAddress)
                 }).then(
-                function(valuePaid) {
+                function (valuePaid) {
                     var msgResult
                     if (valuePaid.toNumber() == ticketPrice) {
                         msgResult = 'Purchase successful'
@@ -164,30 +155,30 @@ window.App = {
                     }
                     $('#buyTicketResult').html(msgResult)
                 }).then(
-                function() {
+                function () {
                     $('#confBalance').html(getBalance(conference.address))
                 })
-        }).catch(function(e) {
+        }).catch(function (e) {
             console.log(e)
         })
     },
 
-    buyMultipleTickets: function(buyerAddress, ticketPrice, ticketcount) {
+    buyMultipleTickets: function (buyerAddress, ticketPrice, ticketcount) {
         var self = this
-        Conference.deployed().then(function(instance) {
+        Conference.deployed().then(function (instance) {
             conference = instance
             conference.buyMultipleTickets(ticketcount, {
                 from: buyerAddress,
                 value: ticketcount * ticketPrice
             }).then(
-                function() {
+                function () {
                     return conference.numRegistrants.call()
                 }).then(
-                function(num) {
+                function (num) {
                     $('#numRegistrants').html(num.toNumber())
                     return conference.registrantsPaid.call(buyerAddress)
                 }).then(
-                function(valuePaid) {
+                function (valuePaid) {
                     var msgResult
                     if (valuePaid.toNumber() == ticketcount * ticketPrice) {
                         msgResult = 'Purchase successful'
@@ -196,22 +187,22 @@ window.App = {
                     }
                     $('#buyTicketsResult').html(msgResult)
                 }).then(
-                function() {
+                function () {
                     $('#confBalance').html(getBalance(conference.address))
                 })
-        }).catch(function(e) {
+        }).catch(function (e) {
             console.log(e)
         })
     },
 
-    refundTicket: function(buyerAddress, ticketPrice) {
+    refundTicket: function (buyerAddress, ticketPrice) {
         var self = this
-        Conference.deployed().then(function(instance) {
+        Conference.deployed().then(function (instance) {
             conference = instance
             var msgResult
 
             conference.registrantsPaid.call(buyerAddress).then(
-                function(result) {
+                function (result) {
                     if (result.toNumber() == 0) {
                         $('#refundTicketResult').html('Buyer is not registered - no refund!')
                     } else {
@@ -219,14 +210,14 @@ window.App = {
                             ticketPrice, {
                                 from: accounts[0]
                             }).then(
-                            function() {
+                            function () {
                                 return conference.numRegistrants.call()
                             }).then(
-                            function(num) {
+                            function (num) {
                                 $('#numRegistrants').html(num.toNumber())
                                 return conference.registrantsPaid.call(buyerAddress)
                             }).then(
-                            function(valuePaid) {
+                            function (valuePaid) {
                                 if (valuePaid.toNumber() == 0) {
                                     msgResult = 'Refund successful'
                                 } else {
@@ -234,29 +225,29 @@ window.App = {
                                 }
                                 $('#refundTicketResult').html(msgResult)
                             }).then(
-                            function() {
+                            function () {
                                 $('#confBalance').html(getBalance(conference.address))
                             })
                     }
                 })
-        }).catch(function(e) {
+        }).catch(function (e) {
             console.log(e)
         })
     }, // end of refund
 
-    setRating: function(buyerAddress, rating) {
+    setRating: function (buyerAddress, rating) {
         var self = this
         var initial_rating = rating
         console.log('initial rating: ' + initial_rating)
-        Conference.deployed().then(function(instance) {
+        Conference.deployed().then(function (instance) {
             conference = instance
             conference.setRating(rating, {
                 from: buyerAddress,
             }).then(
-                function() {
+                function () {
                     return conference.ratingGiven.call(buyerAddress)
                         .then(
-                            function(contract_rating) {
+                            function (contract_rating) {
                                 var msgResult
                                 console.log('contract rating: ' + contract_rating.toNumber())
                                 if (contract_rating.toNumber() == initial_rating) {
@@ -267,24 +258,24 @@ window.App = {
                                 }
                                 $('#ratingResult').html(msgResult)
                             })
-                }).catch(function(e) {
+                }).catch(function (e) {
                 console.log(e)
             })
         })
     },
 
-    getVal: function(rating) {
+    getVal: function (rating) {
         var self = this
         var count
-        Conference.deployed().then(function(instance) {
+        Conference.deployed().then(function (instance) {
             conference = instance
             conference.ratings.call(rating)
                 .then(
-                    function(cnt) { // TODO: Find a way to make this happen synchronously
+                    function (cnt) { // TODO: Find a way to make this happen synchronously
                         console.log('cnt: ' + cnt.toNumber())
                         count = cnt
                     })
-        }).catch(function(e) {
+        }).catch(function (e) {
             console.log(e)
         })
 
@@ -292,65 +283,67 @@ window.App = {
         return count
     },
 
-    destroyContract: function() {
+    destroyContract: function () {
         var self = this
-        Conference.deployed().then(function(instance) {
+        Conference.deployed().then(function (instance) {
             conference = instance
             conference.destroy({
-                    from: accounts[0]
-                }).then(
-                    function() {
-                        $('#destroyContractResult').html('contract destroyed. pls refresh page to reflect balance')
-                    }) // end of conference destroy
-        }).catch(function(e) {
+                from: accounts[0]
+            }).then(
+                function () {
+                    $('#destroyContractResult').html('contract destroyed. pls refresh page to reflect balance')
+                }) // end of conference destroy
+        }).catch(function (e) {
             console.log(e)
         })
     },
 
 }
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     if (typeof web3 !== 'undefined') {
         console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-            // Use Mist/MetaMask's provider
+        // Use Mist/MetaMask's provider
         window.web3 = new Web3(web3.currentProvider)
     } else {
         console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask")
-            // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
         window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
     }
 
-    Conference.setProvider(web3.currentProvider)
+    ParkingLot.setProvider(web3.currentProvider)
     App.start()
 
     // Wire up the UI elements
-    $('#changeQuota').click(function() {
+    $('#changeQuota').click(function () {
         var val = $('#confQuota').val()
         App.changeQuota(val)
     })
-    $('#changeLocation').click(function() {
+
+    $('#changeLocation').click(function () {
         var val = $('#selectLocation').find(':selected').text()
         App.changeLocation(val)
     })
 
-    $('#buyTicket').click(function() {
-        var val = $('#ticketPrice').val()
-        var buyerAddress = $('#buyerAddress').val()
-        App.buyTicket(buyerAddress, web3.toWei(val))
+    $('#addVehicle').click(function () {
+        var vNum = $('#vnum').val()
+        var owner = $('#owner').val()
+        App.addVehicle(owner, vNum)
     })
-    $('#buyTickets').click(function() {
+
+    $('#buyTickets').click(function () {
         var val = $('#ticketPrice').val()
         var ticketcount = $('#ticketsCount').val()
         var buyerAddress = $('#mbuyerAddress').val()
         App.buyMultipleTickets(buyerAddress, web3.toWei(val), ticketcount)
     })
-    $('#refundTicket').click(function() {
+    $('#refundTicket').click(function () {
         var val = $('#ticketPrice').val()
         var buyerAddress = $('#refBuyerAddress').val()
         App.refundTicket(buyerAddress, web3.toWei(val))
     })
-    $('#setRating').click(function() {
+    $('#setRating').click(function () {
         var val = $('#rating').text()
         var buyerAddress = $('#ratingAddress').val()
         $.drawRatingGraph() // here for testing
@@ -360,7 +353,19 @@ window.addEventListener('load', function() {
         else
             App.setRating(buyerAddress, val)
     })
-    $('#destroyContract').click(function() {
+    $('#destroyContract').click(function () {
         App.destroyContract()
     })
+
+    $('.copyBtn').click(function () {
+        var $row = $(this).closest("tr");
+    })
+
+    $.loadAddresses = function () {
+        var index = 0
+
+        accounts.forEach(function (element) {
+            $('#addressTable').append('<tr><td>' + (index++) + '</td><td>' + element + '</td><td><button class="copyBtn">Copy</button></td></td></tr>')
+        }, this);
+    }
 })
